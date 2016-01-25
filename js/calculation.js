ce8309254f816a01482fdf007faa26bf7e51aae5
Varun Dey <varun.silversurfer@gmail.com>
2016-01-25 23:07:10 +0530

    credit box can now take only numerical value and added cdn

--- js/calculation.js
+++ js/calculation.js
@@ -69,3 +69,10 @@ console.log(numRows);
             }
         });
     }
+
+    function checkInput(ob) {
+  var invalidChars = /[^0-9]/gi
+  if(invalidChars.test(ob.value)) {
+            ob.value = ob.value.replace(invalidChars,"");
+      }
+}
