3c2a8c58911e872325df323a9369717575b21e33
Varun Dey <varun.silversurfer@gmail.com>
2016-01-29 19:53:10 +0530

    material is the new sexy. Kudos to @sadarshannaiynar :smile:

--- local/js/calculation.js
+++ local/js/calculation.js
@@ -14,7 +14,8 @@ $(document).ready(function(){
             		if (w == "A" || w == 'a' || w == '9') { w = 9; }
             		if (w == "B" || w == 'b' || w == '8') { w = 8; }
             		if (w == "C" || w == 'c' || w == '7') { w = 7; }
-            		if (w == "D" || w == 'd' || w == '6') { w = 6; }
+            		if (w == "D" || w == 'd' || w == '5') { w = 5; }   
+            if (w == "U" || w == 'u' || w == 'I'|| w == 'i'|| w == 'W'|| w == 'w') { w = 0; } 
             		var mult = (l * w);
             		$(this).find('input.row-total').val(mult ? mult : "");
             		gr += isNumber(w) ? parseInt(w, 10) : 0;
@@ -44,7 +45,7 @@ $(document).ready(function(){
     	}
 
     	$(function() {
-        	$("#gpa").on("click", ".calculation", recalc);
+        	$("#gpa").on("keyup", ".calculation", recalc);
         	$("#gpa").on("keyup blur", ".form-control", recalc);
         	$("#gpa").on("keyup", ".grade:last", function() { addRow(); } );
         	$("#add_row").on("click", function() { addRow() });
